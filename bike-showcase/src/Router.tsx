ee93edf41855e1e0eb85d0ab8e7305c2853d2dfa
giovanni <giovanni_ge@hotmai.com>
2023-03-01 11:52:59 -0300

    feat: adicionando nova rota about

--- bike-showcase/src/Router.tsx
+++ bike-showcase/src/Router.tsx
@@ -2,12 +2,14 @@ import { Routes } from "react-router-dom"
 import { Route } from "react-router"
 import { DefaultLayout } from "./layouts/DefaultLayout"
 import { Main } from "./Main/Main"
+import { About } from "./pages/about"
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
   )
