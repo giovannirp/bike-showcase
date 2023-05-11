4258c3b815bf52cba28e2b7ed62a0bc62883c0c5
giovanni <giovanni_ge@hotmai.com>
2023-05-10 23:04:14 -0300

    feat: criando tabela de transição

--- bike-showcase/src/pages/Transations/index.tsx
+++ bike-showcase/src/pages/Transations/index.tsx
@@ -1,9 +1,36 @@
-import { ContainerTransations } from "./style";
+import { useContext } from "react";
+import { ContainerTransactions, TrasactionsTable, TrasictionsHead } from "./style";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dataFormater } from "../../ultis/formatters";
 
 export function Transations() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
-    <ContainerTransations>
+    <ContainerTransactions>
       <h1>Bem vindo nas transições</h1>
-    </ContainerTransations>
+      <TrasactionsTable>
+        <TrasictionsHead>
+          <tr>
+            <td>Produto</td>
+            <td>Preço</td>
+            <td>Categoria</td>
+            <td>Data</td>
+          </tr>
+        </TrasictionsHead>
+        <tbody>
+          {transactions.map((listProduct) => {
+            return (
+              <tr key={listProduct.id}>
+                <td>{listProduct.description}</td>
+                <td>{listProduct.price}</td>
+                <td>{listProduct.category}</td>
+                <td>{dataFormater.format(new Date(listProduct.createdAt))}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </TrasactionsTable>
+    </ContainerTransactions>
   )
 }
--- bike-showcase/src/pages/Transations/style.ts
+++ bike-showcase/src/pages/Transations/style.ts
@@ -1,10 +1,44 @@
 import styled from "styled-components";
 
-export const ContainerTransations = styled.section`
+export const ContainerTransactions = styled.section`
+  width: 100%;
+  margin-left: 25px;
+  margin-right: 25px;
 
   h1 {
     font-size: 22px;
-    padding: 20px;
+    padding: 20px 0 0px 5px;
     color: ${(props) => props.theme["gray-900"]};
+  }
+`;
+
+export const TrasactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 .5rem;
+  margin-top: 1.5rem;
+  max-width: 1070px;
+
+  td {
+    padding: 1.25rem 2rem;
+    border: 1px solid ${(props) => props.theme["gray-300"]};
+    color: ${(props) => props.theme["gray-900"]};
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+export const TrasictionsHead = styled.thead`
+  background: ${(porps) => porps.theme["green-700"]};
+
+  td {
+    color: ${(props) => props.theme.white}!important;
   }
 `;
